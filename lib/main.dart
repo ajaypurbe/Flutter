916271d59584345c20c846768b9d5a62c91e265b
unknown <purbea@uni.coventry.ac.uk>
2021-12-21 23:59:01 +0545

    add numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:si_calculator/screen/sicalculator.dart';
+import 'package:si_calculator/screen/addnumber.dart';
+//import 'package:si_calculator/screen/sicalculator.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SiCalculator(),
+      home: AddNumber(),
     );
   }
 }
