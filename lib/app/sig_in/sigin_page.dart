a6342bb8ddeea8f3a0e983130f0e37a386dc55b7
unknown <purweaj766@gmail.com>
2022-05-20 01:23:10 +0545

    upto padding and use of crossaxisalinment

--- lib/app/sig_in/sigin_page.dart
+++ lib/app/sig_in/sigin_page.dart
@@ -6,7 +6,35 @@ class SigInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Time Tracker")),
+      appBar: AppBar(
+        title: Text("Time Tracker"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.amber.shade500,
+              child: const SizedBox(
+                height: 100,
+              ),
+            ),
+            Container(
+              color: Color.fromARGB(255, 17, 193, 46),
+              child: const SizedBox(
+                height: 100,
+              ),
+            ),
+            Container(
+              color: Color.fromRGBO(36, 7, 255, 1),
+              child: const SizedBox(
+                height: 100,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: SigInPage(
-        
-      ),
+      home: const SigInPage(),
     );
   }
 }
